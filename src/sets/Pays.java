35e887666cabafaab55e3fa8dc33ebdf14c401d5
tgentreau <gentreau.thomas@gmail.com>
2022-06-10 11:48:44 +0200

    tp-enum ex2

--- src/sets/Pays.java
+++ src/sets/Pays.java
@@ -1,5 +1,7 @@
 package sets;
 
+import testenumeration.Continent;
+
 import java.util.HashSet;
 import java.util.Locale;
 
@@ -7,11 +9,13 @@ public class Pays {
     public String nom;
     public double nbrHabitant;
     public double pibParHabitant;
+    public Continent continent;
 
-    public Pays(String nom, double nbrHabitant, double pibParHabitant) {
+    public Pays(String nom, double nbrHabitant, double pibParHabitant, Continent continent) {
         this.nom = nom;
         this.nbrHabitant = nbrHabitant;
         this.pibParHabitant = pibParHabitant;
+        this.continent = continent;
     }
 
     double calculerPIBTotal() {
@@ -24,20 +28,21 @@ public class Pays {
                 "nom='" + nom + '\'' +
                 ", nbrHabitant=" + nbrHabitant +
                 ", pibParHabitant=" + pibParHabitant +
+                ", continent=" + continent +
                 '}';
     }
 
     public static void main(String[] args) {
         HashSet<Pays> pays = new HashSet<>();
-        Pays usa = new Pays("USA", 329500000, 63543.58);
-        Pays france = new Pays("France", 67390000, 38625.07);
-        Pays allemagne = new Pays("Allemagne", 83240000, 45723.64);
-        Pays uk = new Pays("Uk", 67220000, 40284.64);
-        Pays italie = new Pays("Italie", 59550000, 31676.20);
-        Pays japon = new Pays("Japon", 125800000, 40113.06);
-        Pays chine = new Pays("Chine", 1402000000, 10500.40);
-        Pays russie = new Pays("Russie", 144100000, 10126.72);
-        Pays inde = new Pays("Inde", 1380000000, 1900.71);
+        Pays usa = new Pays("USA", 329500000, 63543.58, Continent.AMERIQUE);
+        Pays france = new Pays("France", 67390000, 38625.07, Continent.EUROPE);
+        Pays allemagne = new Pays("Allemagne", 83240000, 45723.64, Continent.EUROPE);
+        Pays uk = new Pays("Uk", 67220000, 40284.64, Continent.EUROPE);
+        Pays italie = new Pays("Italie", 59550000, 31676.20, Continent.EUROPE);
+        Pays japon = new Pays("Japon", 125800000, 40113.06, Continent.ASIE);
+        Pays chine = new Pays("Chine", 1402000000, 10500.40, Continent.ASIE);
+        Pays russie = new Pays("Russie", 144100000, 10126.72, Continent.EUROPE);
+        Pays inde = new Pays("Inde", 1380000000, 1900.71, Continent.ASIE);
         pays.add(usa);
         pays.add(france);
         pays.add(allemagne);
@@ -73,11 +78,29 @@ public class Pays {
                 }
             }
         }
-        pays.remove(paysAvecPibTotalMin);
+
+        int compteurEU = 0;
+        int compteurAS = 0;
+        int compteurAM = 0;
+
+        for (Pays pay : pays) {
+            if(pay.continent.equals(Continent.EUROPE)) {
+                compteurEU++;
+            } else if(pay.continent.equals(Continent.AMERIQUE)) {
+                compteurAM++;
+            } else if(pay.continent.equals(Continent.ASIE)) {
+                compteurAS++;
+            }
+        }
+//        pays.remove(paysAvecPibTotalMin);
         System.out.println(paysAvecPibMax.toString());
         System.out.println(paysAvecPibTotalMax.toString());
         System.out.println(paysAvecPibTotalMin.toString());
         System.out.println(pays.toString());
+        System.out.println();
+        StringBuilder builder = new StringBuilder();
+        builder.append("Europe = ").append(compteurEU).append("\n").append("Amerique = ").append(compteurAM).append("\n").append("Asie = ").append(compteurAS);
+        System.out.println(builder.toString());
 
     }
 }
